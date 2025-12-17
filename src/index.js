f3e79ebf627478fdf15468dee7ca13ba600bf41b
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2025-12-17 11:09:03 +0100

    add the switch page logic to nav buttons

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import { loadHome } from './home.js';
 import { loadMenu } from './menu.js';
 import { loadAbout } from './about.js';
 
+loadHome();
+
+document.querySelector('#homeButton').addEventListener('click', () => {
+    loadHome();
+});
+
+document.querySelector('#menuButton').addEventListener('click', () => {
+    loadMenu();
+});
+
+document.querySelector('#aboutButton').addEventListener('click', () => {
+    loadAbout();
+});
